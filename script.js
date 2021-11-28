635b7612ef685abfbc9ed29bb996f9ce47fdec42
magithomas <magithomas993@gmail.com>
2021-11-28 21:37:21 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let displayValue=(num)=>text1.value+=num
+//function  displayValue(num){
+   // let txt_box=document.querySelector("#text1")
+    //txt_box.value+=num
+//}
+let clearBox=()=>text1.value=""
+//function clearBox(){
+   // document.querySelector("#text1").value=""
+//}
+let evaluateExpression=()=>text1.value=eval(text1.value)
+//function evaluateExpression(){
+    //let txt_box=document.querySelector("#text1")
+    //txt_box.value=eval(txt_box.value)
+//}
+let removeElement=()=>text1.value=text1.value.slice(0,-1)
+//function removeElement(){
+    //let txt_box=document.querySelector("#text1")
+    //txt_box.value=txt_box.value.slice(0,-1)
+//}
+
